6a0d29cdf67ce264670ba3511a7b3c03d118c496
alexust27 <ustinov1998s@gmail.com>
2022-05-15 17:11:24 +0300

    work commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Запуск
+windows 
+`` python.exe .\heatmap_compare.py -c image1.png image2.png ``
+
+linux
--- heatmap_compare.py
+++ heatmap_compare.py
@@ -7,7 +7,6 @@ import argparse
 
 def make_dir(path):
     if not os.path.exists(path):
-        path = os.path.abspath(os.path.dirname(path))
         os.mkdir(path)
     return os.path.abspath(path)
 
@@ -15,12 +14,16 @@ def make_dir(path):
 PATH_TO_IMGS = make_dir("images")
 PATH_TO_GRAY_IMGS = make_dir(os.path.join(PATH_TO_IMGS, "gray"))
 RESULT_DIR = make_dir("res")
+RESULT_HM_DIR = make_dir(os.path.join(RESULT_DIR, "heatmaps"))
+RESULT_DIF_DIR = make_dir(os.path.join(RESULT_DIR, "diff"))
+
 N_GRAY = 1
 diff_save_path = ""
 need_save_gray = False
 need_save_heat_map = True
 file1 = ""
 file2 = ""
+COLORMAP = 'seismic'
 
 
 def make_diff(ar1, ar2):
@@ -67,14 +70,14 @@ def make_heat_map(arr, ar1=None, ar2=None):
           "ytick.labelleft": False,
           "ytick.left": False}
     plt.rcParams.update(rc)
-    fig = plt.figure(figsize=(13, 4))
+    fig = plt.figure(figsize=(4, 4))
     # fig = plt.figure()
-    ax = fig.add_subplot(1, 3, 3)
+    ax = fig.add_subplot(1, 1, 1)
 
     # ax2 = fig.add_subplot(1, 4, 4)
     # ax = img1.gca()
     # img = ax.matshow(arr, cmap='RdYlGn_r')
-    img = ax.imshow(arr, cmap='RdBu_r', aspect='auto')
+    img = ax.imshow(arr, cmap=COLORMAP, aspect='auto')
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size='5%', pad=0.1)
     cbar = plt.colorbar(img, cax=cax, ax=ax)
@@ -83,18 +86,19 @@ def make_heat_map(arr, ar1=None, ar2=None):
     plt.title("Heatmap", loc='right')
     # plt.colorbar(img, ax=ax, format='%')
 
-    if ar1 is not None and ar2 is not None:
-        img2 = fig.add_subplot(1, 3, 1)
-        img2.imshow(ar1, cmap='gray', aspect='auto')
-        plt.title(file1 if file1 else "first")
-        img3 = fig.add_subplot(1, 3, 2)
-        img3.imshow(ar2, cmap='gray', aspect='auto')
-        plt.title(file2 if file2 else "second")
+    # if ar1 is not None and ar2 is not None:
+    #     img2 = fig.add_subplot(1, 3, 1)
+    #     img2.imshow(ar1, cmap='gray', aspect='auto')
+    #     plt.title(file1 if file1 else "first")
+    #     img3 = fig.add_subplot(1, 3, 2)
+    #     img3.imshow(ar2, cmap='gray', aspect='auto')
+    #     plt.title(file2 if file2 else "second")
 
         # fig.suptitle('Heatmaps with `Axes.matshow`', fontsize=16)
-    if need_save_heat_map:
-        plt.savefig(os.path.join(RESULT_DIR, "heatmap.jpg"))
-    plt.show()
+    res_heatmap = os.path.join(RESULT_HM_DIR, f"{file1}_{file2}.png")
+    plt.savefig(res_heatmap)
+    return res_heatmap
+    # plt.show()
 
 
 def make_heat_map_from_gray(gray_image, gray_image2):
@@ -103,19 +107,21 @@ def make_heat_map_from_gray(gray_image, gray_image2):
     diff_arr = make_diff(ar1, ar2)
     if diff_save_path:
         cv2.imwrite(diff_save_path, diff_arr)
-    make_heat_map(diff_arr, ar1, ar2)
+    return make_heat_map(diff_arr, ar1, ar2)
 
 
 def make_two_gray(gray_file, gray_file2):
-    global file1, file2
+    global file1, file2, diff_save_path
     if not os.path.isfile(gray_file2) or not os.path.isfile(gray_file):
         print("File " + gray_file + " or " + gray_file2 + " not exist")
         return
     file1 = get_file_name(gray_file)
     file2 = get_file_name(gray_file2)
+    diff_save_path = os.path.join(RESULT_DIF_DIR, f"{file1}_{file2}.png")
+
     gray_image = cv2.imread(gray_file, cv2.IMREAD_GRAYSCALE)
     gray_image2 = cv2.imread(gray_file2, cv2.IMREAD_GRAYSCALE)
-    make_heat_map_from_gray(gray_image, gray_image2)
+    return make_heat_map_from_gray(gray_image, gray_image2)
 
 
 def make_with_origin(origin_file):
@@ -134,7 +140,7 @@ def make_with_origin(origin_file):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--origin', type=str, help='Path to origin file')
-    parser.add_argument('-c', '--gray_img', type=str, nargs=2, help='Paths to 2 gray files')
+    parser.add_argument('-c', '--compare', type=str, nargs=2, help='Paths to 2 gray files')
     parser.add_argument('-m', '--heat_map', type=str, help='Paths to diff file')
     parser.add_argument('-s', '--save', type=str, help='Path for save file')
     args = vars(parser.parse_args())
@@ -142,8 +148,8 @@ if __name__ == '__main__':
         diff_save_path = os.path.join(RESULT_DIR, args['save'] + "_diff.jpg")
     if args['origin']:
         make_with_origin(args['origin'])
-    elif args['gray_img']:
-        make_two_gray(args['gray_img'][0], args['gray_img'][1])
+    elif args['compare']:
+        make_two_gray(args['compare'][0], args['compare'][1])
     elif args['heat_map']:
         diff_img = cv2.imread(args['heat_map'], cv2.IMREAD_GRAYSCALE)
         make_heat_map(diff_img)
